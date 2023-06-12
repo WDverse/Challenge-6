9d33427b5b54ba733becd646a105f5f839ff6337
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-06-11 22:25:17 -0400

    updated class in html and created variables and funcs in js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var inputEl = document.getElementById('search-city');
+var searchBtn = document.getElementById('search-btn');
+var todayDiv = document.getElementById('weather-today');
+var fiveDayDiv = document.getElementById('5-day-forecast');
+
+
+function getApi (){
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d6e36cd62e6cfcf651eefb5f7aa8d3dd}'
+
+    fetch(requestUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        
+    })
+
+}
