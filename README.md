95bf82b3ed71f2a161157a46f55fcc325a2c2870
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-30 22:51:33 -0400

    added style, comments and updated readme

--- README.md
+++ README.md
@@ -1,102 +1,62 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Weather Dashboard
 
-## Your Task
+## Description
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+This is front-end application is weather dashboard that runs in the browser and features dynamically updated HTML and CSS. It shows the present and future weather conditions for a search city.
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+## Table of Contents
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+- [Installation](#installation)
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+- [Usage](#usage)
 
-## User Story
+- [License](#license)
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+- [Credit](#credit)  
 
-## Acceptance Criteria
+- [APIs](#apis)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+- [Deployment](#deployment)
 
-## Mock-Up
+- [References](#references)
 
-The following image shows the web application's appearance and functionality:
+## Installation
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+N/A
 
-## Grading Requirements
+## Usage
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+Open the application in the browser, enter the name of the city you want to search and click the search button. The weather info is dynamically generated and can be seen on the screen. To search another city, refresh the browser and repeat the steps.
 
-This Challenge is graded based on the following criteria: 
+The following image shows the application's functionality:  
+![Screenshot](./assets/images/screenshot.png)
 
-### Technical Acceptance Criteria: 40%
+## License
 
-* Satisfies all of the above acceptance criteria plus the following:
+N/A
 
-    * Uses the OpenWeather API to retrieve weather data.
+## Credit
 
-    * Uses `localStorage` to store persistent data.
+AskBCS tutors
 
-### Deployment: 32%
+Dane Shrewsbury
 
-* Application deployed at live URL.
+Added comments with the help of ChatGPT
 
-* Application loads with no errors.
+## APIs
 
-* Application GitHub URL submitted.
+[5 Day Weather Forecast](https://openweathermap.org/forecast5)
 
-* GitHub repository that contains application code.
+## Deployment
 
-### Application Quality: 15%
+Here is the link to the web application:
 
-* Application user experience is intuitive and easy to navigate.
+[Weather Dashboard](https://wdverse.github.io/Weather-Dashboard/)
 
-* Application user interface style is clean and polished.
+## References
 
-* Application resembles the mock-up functionality provided in the Challenge instructions.
+Course material
 
-### Repository Quality: 13%
+W3 Schools
 
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+[Coolors](https://coolors.co/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,176 @@
-var inputEl = document.getElementById('search-city');
-var searchBtn = document.getElementById('search-btn');
-var todayDiv = document.getElementById('weather-today');
-var forecastDiv = document.getElementById('five-day-forecast');
-var apiKey = 'd6e36cd62e6cfcf651eefb5f7aa8d3dd';
-var weatherToday = document.getElementById('weather-info');
-var forecastHeader = document.getElementById('forecast')
-var currentDay = dayjs().format('DD/MM/YYYY')
-var currentDayEl = document.getElementById('current-day');
-forecastHeader.style.display = 'none';
+// Get references to HTML elements by their IDs
+var inputEl = document.getElementById("search-city"); // Input field for city search
+var searchBtn = document.getElementById("search-btn"); // Search button
+var todayDiv = document.getElementById("todayDiv"); // Container for current weather information
+var forecastDiv = document.getElementById("five-day-forecast"); // Container for 5-day forecast
+var apiKey = "d6e36cd62e6cfcf651eefb5f7aa8d3dd"; // API key for weather data
+var weatherInfo = document.getElementById("weather-info"); // List for weather information
+var forecastHeader = document.getElementById("forecast"); // Header for 5-day forecast
+var currentDay = dayjs().format("DD/MM/YYYY"); // Get the current date in the specified format
+var currentDayEl = document.getElementById("current-day"); // Element to display the current date
+
+// Hide the 5-day forecast header initially
+forecastHeader.style.display = "none";
+
+// Extend the functionality of the Day.js library with plugins
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
+// Hide the container for current weather information initially
+todayDiv.style.display = "none";
+
+// Function to fetch weather data by latitude and longitude
 function getApi(lat, lon) {
-    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+  // Construct the URL for the OpenWeatherMap API request
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('weather', data)
-            renderWeather(data.list)
-            renderForecast(data.list);
-        })
-
-
+  // Fetch data from the API
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json(); // Parse the response as JSON
+    })
+    .then(function (data) {
+      console.log("weather", data); // Log the weather data to the console
+      renderWeather(data.list); // Call a function to render the current weather
+      renderForecast(data.list); // Call a function to render the 5-day forecast
+    });
 }
+
+// Function to get coordinates (latitude and longitude) for a given city
 function getCoords(city) {
-    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+  // Construct the URL for the OpenWeatherMap API request to get coordinates
+  var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('data', data[0].lat)
-            getApi(data[0].lat, data[0].lon);
-
-        })
+  // Fetch data from the API
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json(); // Parse the response as JSON
+    })
+    .then(function (data) {
+      console.log("data", data[0].lat); // Log the latitude of the city
+      // Call the getApi function with the retrieved latitude and longitude
+      getApi(data[0].lat, data[0].lon);
+    });
 }
 
+// Function to render the current weather information
 function renderWeather(data) {
-    weatherToday.innerHTML = '';
-    currentDayEl.textContent = inputEl.value.trim() + " " + currentDay;
-    var currentIcon = document.getElementById('current-icon');
-    currentIcon.src = `https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`
-    console.log('icon: ', `${data[0].weather[0].icon}`);
-    currentDayEl.append(currentIcon);
-    var tempEl = document.createElement('li');
-    tempEl.textContent = `Temp: ${data[0].main.temp} `
-    var humidityEl = document.createElement('li');
-    humidityEl.textContent = `Humidity: ${data[0].main.humidity}% `
-    var windSpeedEl = document.createElement('li');
-    windSpeedEl.textContent = `Wind: ${data[0].wind.speed} MPH `
-    weatherToday.append(tempEl, humidityEl, windSpeedEl);
+  // Clear any previous weather information from the weatherInfo element
+  weatherInfo.innerHTML = "";
 
+  // Display the container for current weather information
+  todayDiv.style.display = "block";
 
+  // Set the text content of currentDayEl to show the city name and current date
+  currentDayEl.textContent = inputEl.value.trim() + " " + currentDay;
+
+  // Create an element to display the weather icon
+  var currentIcon = document.getElementById("current-icon");
+  currentIcon.classList.add("icon");
+
+  // Set the src attribute of the currentIcon element to display the weather icon
+  currentIcon.src = `https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`;
+
+  // Log the weather icon to the console
+  console.log("icon: ", `${data[0].weather[0].icon}`);
+
+  // Append the currentIcon element to currentDayEl to display the weather icon
+  currentDayEl.append(currentIcon);
+
+  // Create elements to display temperature, humidity, and wind speed
+  var tempEl = document.createElement("li");
+  tempEl.textContent = `Temp: ${data[0].main.temp} ℃ `;
+
+  var humidityEl = document.createElement("li");
+  humidityEl.textContent = `Humidity: ${data[0].main.humidity}% `;
+
+  var windSpeedEl = document.createElement("li");
+  windSpeedEl.textContent = `Wind: ${data[0].wind.speed} MPH `;
+
+  // Append temperature, humidity, and wind speed elements to weatherInfo
+  weatherInfo.append(tempEl, humidityEl, windSpeedEl);
 }
-
+// Function to handle the form submission when the search button is clicked
 function searchFormSubmit(event) {
-    forecastDiv.innerHTML = '5-Day Forecast';
-    // console.log(event);
-    event.preventDefault();
-    var cityInput = inputEl.value.trim();
-    // console.log('cityInput', cityInput); 
-    getCoords(cityInput);
+  event.preventDefault(); // Prevent the default form submission behavior (page reload)
+
+  // Get the trimmed value from the input field (city name)
+  var cityInput = inputEl.value.trim();
+
+  // Call the getCoords function to fetch coordinates for the entered city
+  getCoords(cityInput);
 }
 
-searchBtn.addEventListener('click', searchFormSubmit);
-
+// Add an event listener to the search button, listening for a click event
+searchBtn.addEventListener("click", searchFormSubmit);
 
 // Function to display 5 day forecast.
 function renderForecast(dailyForecast) {
-    // Create unix timestamps for start and end of 5 day forecast
-    var startDt = dayjs().add(1, 'day').startOf('day').unix();
-    var endDt = dayjs().add(6, 'day').startOf('day').unix();
-    forecastHeader.style.display = 'block';
+  // Create unix timestamps for start and end of 5 day forecast
+  var startDt = dayjs().add(1, "day").startOf("day").unix(); // Start with tomorrow
+  var endDt = dayjs().add(6, "day").startOf("day").unix(); // End with the day after tomorrow
 
+  // Display the 5-day forecast header
+  forecastHeader.style.display = "block";
 
-    for (var i = 0; i < dailyForecast.length; i++) {
-        forecastHeader.setAttribute('style', 'display: block');
-        console.log(dailyForecast[i].dt)
-        const date = dayjs(dailyForecast[i].dt * 1000)
-        console.log(date)
-        date.format("DD/MM/YYYY");
+  // Loop through the daily forecast data
+  for (var i = 0; i < dailyForecast.length; i++) {
+    // Ensure the forecast header is displayed
+    forecastHeader.setAttribute("style", "display: block");
 
-        // First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
-        if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+    // Create a Date object from the Unix timestamp (multiply by 1000 for milliseconds)
+    const date = dayjs(dailyForecast[i].dt * 1000);
 
-            // Then filters through the data and returns only data captured at noon for each day.
-            if (dailyForecast[i].dt_txt.slice(11, 13) == "12") {
-                //   forecastDiv(dailyForecast[i]);
-                var nextDayDiv = document.createElement('div');
-                var futureDate = document.createElement('header');
-                futureDate.textContent = date.format("DD/MM/YYYY");
-                var forecastIcon = document.createElement('img');
-                forecastIcon.src = `https://openweathermap.org/img/wn/${dailyForecast[i].weather[0].icon}@2x.png`
-                var nextDayInfo = document.createElement('ul');
-                var nextDayTemp = document.createElement('li');
-                var nextDayWind = document.createElement('li');
-                var nextDayHumid = document.createElement('li');
+    // Format the date as "DD/MM/YYYY"
+    const formattedDate = date.format("DD/MM/YYYY");
 
-                nextDayTemp.textContent = "Temp: " + dailyForecast[i].main.temp;
-                nextDayWind.textContent = "Wind: " + dailyForecast[i].wind.speed + " MPH";
-                nextDayHumid.textContent = "Humidity: " + dailyForecast[i].main.humidity + "%";
-                nextDayInfo.appendChild(nextDayTemp);
-                nextDayInfo.appendChild(nextDayWind);
-                nextDayInfo.appendChild(nextDayHumid);
-                nextDayDiv.appendChild(futureDate);
-                nextDayDiv.appendChild(forecastIcon);
-                nextDayDiv.appendChild(nextDayInfo);
-                forecastDiv.appendChild(nextDayDiv);
-            }
-        }
+    // First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
+    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+      // Then filters through the data and returns only data captured at noon for each day.
+      // Check if the time of the forecast data is 12:00 (noon)
+      if (dailyForecast[i].dt_txt.slice(11, 13) == "12") {
+        // Create a new div element for the next day's forecast
+        var nextDayDiv = document.createElement("div");
+        nextDayDiv.classList.add("nextDay"); // Add a CSS class to the div
+
+        // Create a header element to display the future date
+        var futureDate = document.createElement("header");
+        futureDate.textContent = date.format("DD/MM/YYYY"); // Set the text content with the formatted date
+        futureDate.classList.add("futureDate"); // Add a CSS class to the header
+
+        // Create an image element for the forecast icon
+        var forecastIcon = document.createElement("img");
+        // Set the src attribute of the forecast icon based on the weather icon code
+        forecastIcon.src = `https://openweathermap.org/img/wn/${dailyForecast[i].weather[0].icon}@2x.png`;
+
+        // Create an unordered list element for the next day's weather information
+        var nextDayInfo = document.createElement("ul");
+
+        // Create list items for temperature, wind, and humidity
+        var nextDayTemp = document.createElement("li");
+        var nextDayWind = document.createElement("li");
+        var nextDayHumid = document.createElement("li");
+
+        // Set the text content for temperature, wind, and humidity
+        nextDayTemp.textContent = "Temp: " + dailyForecast[i].main.temp + " ℃";
+        nextDayWind.textContent =
+          "Wind: " + dailyForecast[i].wind.speed + " MPH";
+        nextDayHumid.textContent =
+          "Humidity: " + dailyForecast[i].main.humidity + "%";
+
+        // Append the temperature, wind, and humidity list items to the nextDayInfo
+        nextDayInfo.appendChild(nextDayTemp);
+        nextDayInfo.appendChild(nextDayWind);
+        nextDayInfo.appendChild(nextDayHumid);
+
+        // Append the future date, forecast icon, and weather information to the nextDayDiv
+        nextDayDiv.appendChild(futureDate);
+        nextDayDiv.appendChild(forecastIcon);
+        nextDayDiv.appendChild(nextDayInfo);
+
+        // Append the nextDayDiv to the forecastDiv to display the next day's forecast
+        forecastDiv.appendChild(nextDayDiv);
+      }
     }
-
+  }
 }
